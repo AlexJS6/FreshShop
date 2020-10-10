fe43f075c2a684bf7e783ab7948b074319748e4b
AlexJS6 <alex.spiesberger@gmail.com>
2020-10-10 14:59:04 +0200

    Adding authentication from OC

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
@@ -29,6 +30,7 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 
 app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
 
 
